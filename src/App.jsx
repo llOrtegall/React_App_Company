eaa46a28ede996ad4c5e666a592988ce2fdc0444
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-21 09:45:18 -0500

    creando el login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { Login } from "./Login";
 import { Register } from "./Register";
 
 export function App() {
+
+  const [currentForm, setCurrentForm] = useState('login')
+  const toggleForm = (forName) => {
+    setCurrentForm(forName)
+  }
+
   return (
     <>
-      <Login />
+      {
+        currentForm === 'login' ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </>
   )
 }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,29 @@
-export function Login() {
+import { useState } from "react";
+
+export function Login(props) {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  }
+
   return (
-    <form action="">
-      <label htmlFor="email">email</label>
-      <input type="email" placeholder="correo@correo.com" id="email" name="email" />
-      <label htmlFor="password">password</label>
-      <input type="password" placeholder="*******" id="password" name="password" />
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)}
+          type="email" placeholder="correo@correo.com"
+          id="email" name="email" />
+        <label htmlFor="password">password</label>
+        <input value={password} onChange={(e) => setPassword(e.target.value)}
+          type="password" placeholder="*******"
+          id="password" name="password" />
+        <button>Log In</button>
+      </form>
+      <button onClick={() => props.onFormSwitch('register')} >Don't have an account ? register Here</button>
+    </>
   )
 }
