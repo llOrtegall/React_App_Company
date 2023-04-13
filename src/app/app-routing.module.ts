4332ab7bede7a70d9728158dd153595d39fa8a11
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-13 00:20:14 -0500

    first commit 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PagesHomeComponent } from './modules/home/pages-home/pages-home.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component:PagesHomeComponent, /** llamamos el componente a la vez que hacemos la carga */
-    loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule)
-  },
-  {
-    path: 'auth',
-    loadChildren: () => import(`./modules/auth/auth.module`).then(m => m.AuthModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
