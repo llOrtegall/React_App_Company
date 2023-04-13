1aa30cc169dc168baed1f9bae8cde57f1d9e2b24
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-13 02:32:27 -0500

    quede sin base de datos para implementar

---
+++ src/app/services/crud.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'; /** Esto es de JS */
+import { User } from './User'; /** estructura del empeado */
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  API01: string = 'http://localhost/empleados/'
+
+  constructor(private clientHttp: HttpClient) {
+
+    Add_User(datosEmpleado:Empleado):Observable<any>{
+      return this.clientHttp.post('this.API + ')
+    }
+  }
+}
