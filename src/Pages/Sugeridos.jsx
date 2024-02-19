dd0ea9d17274fe1bbf4e89ac4f132b4b2ec0dca2
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-19 10:01:47 -0500

    componentes terminados

--- src/Pages/Sugeridos.jsx
+++ src/Pages/Sugeridos.jsx
@@ -1,21 +1,30 @@
-import { determineProgressColor } from '../utils/progress.js'
-import { useSugeridos } from '../hooks/useSugeridos.js'
-import { ProgressSugerido } from '../components/ProgressSugerido.jsx'
 import { ConsultarBoletasGanadas } from '../components/ConsultarBoletasGanadas.jsx'
+import { ProgressSugerido } from '../components/ProgressSugerido.jsx'
+import { determineProgressColor } from '../utils/progress.js'
+import { useSugeridos2 } from '../hooks/useSugeridos2.js'
+import { useSugeridos } from '../hooks/useSugeridos.js'
 
 export function Sugeridos ({ zone, user }) {
   const { data, MetaSugerido, ImpresosActual, percentage } = useSugeridos({ zone, user })
+  const { data2, ImpresosActual2, MetaSugerido2, percentage2 } = useSugeridos2({ zone, user })
   const color = determineProgressColor(percentage)
 
   return (
     <section>
       <h1 className='text-center text-3xl font-semibold py-2'>Sugeridos Del Día </h1>
 
-      <div className='grid grid-cols-1 gap-4'>
+      <div className='grid grid-cols-1'>
         <ProgressSugerido percentage={percentage} data={data} MetaSugerido={MetaSugerido} ImpresosActual={ImpresosActual} color={color} user={user} />
+        {
+          data2
+            ? (
+              <ProgressSugerido percentage={percentage2} data={data2} MetaSugerido={MetaSugerido2} ImpresosActual={ImpresosActual2} color={determineProgressColor(percentage2)} user={user} />
+              )
+            : null
+        }
       </div>
 
-      <div className='pt-4'>
+      <div className='py-2'>
         <ConsultarBoletasGanadas codigo={user.codigo} user={user} />
       </div>
 
---
+++ src/hooks/useSugeridos2.js
@@ -0,0 +1,55 @@
+import { calcularPorcentaje } from '../utils/progress'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const productMetaMap = {
+  CHANCE: 1000,
+  PAGAMAS: 1500,
+  CHOLADITO: 650,
+  DOBLECHANCE: 4000,
+  PATA: 3000,
+  CHANCE_MILLONARIO: 5000,
+  KASH: 500,
+  GANE5: 1000
+}
+
+function DeterminarMeta (product, meta) {
+  const divisor = productMetaMap[product]
+  if (divisor) {
+    return Math.floor(meta / divisor * 2)
+  }
+  return Math.floor(meta)
+}
+
+export const useSugeridos2 = ({ zone, user }) => {
+  const { usuario, codigo } = user
+  const [data2, setData2] = useState(null)
+  const [error2, setError2] = useState(null)
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios.post('/SugeridosSegundaConsulta', { codigo, user: usuario, zona: zone })
+        .then((response) => {
+          setData2(response.data)
+        })
+        .catch((error) => {
+          setError2(error.response.data.message)
+        })
+    }
+    fetchData()
+    const intervalId = setInterval(fetchData, 5 * 60 * 1000) // Fetch data every 5 minutes
+    return () => clearInterval(intervalId) // Clear interval on component unmount
+  }, [])
+
+  const MetaSugerido2 = data2 ? DeterminarMeta(data2?.SUGERIDO2, data2?.META_SUGERIDO1) : 0
+  const ImpresosActual2 = data2 ? DeterminarMeta(data2?.SUGERIDO2, data2?.VTA_SUGERIDO) : 0
+
+  const percentage2 = Math.floor(calcularPorcentaje(ImpresosActual2, MetaSugerido2))
+
+  console.log('data2', data2)
+  console.log('ImpresosActual2', ImpresosActual2)
+  console.log('MetaSugerido2', MetaSugerido2)
+  console.log('percentage2', percentage2)
+
+  return { data2, MetaSugerido2, ImpresosActual2, error2, percentage2 }
+}
