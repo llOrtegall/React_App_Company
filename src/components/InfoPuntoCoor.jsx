2cdd677af35d68a5bb58ee940876f2f6d5337dd5
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-24 12:27:49 -0500

    aplicar cambios de hora

--- src/components/InfoPuntoCoor.jsx
+++ src/components/InfoPuntoCoor.jsx
@@ -1,52 +1,48 @@
 import { getColombiaTime } from '../services/HoraColombia'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Cargando } from './animation/Loadin'
 
 export function InfoPuntCoord ({ pdv }) {
   const { NOMBRE, SUPERVISOR, CATEGORIA, VERSION } = pdv
   const [Fecha, setFecha] = useState('')
   const [Hora, setHora] = useState('')
-  const isMounted = useRef(true)
 
   useEffect(() => {
-    getColombiaTime()
-      .then(response => {
-        if (isMounted.current) {
+    const fetchTime = () => {
+      getColombiaTime()
+        .then(response => {
           setFecha(response.fecha)
-          SimularReloj(response.hora)
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching time:', error)
-      })
+          const [timePart, period] = response.hora.split(' ')
+          const [hours, minutes, seconds] = timePart.split(':').map(Number)
+          let date = new Date()
+          date.setHours(hours + (period === 'pm' ? 12 : 0))
+          date.setMinutes(minutes)
+          date.setSeconds(seconds)
+
+          const intervalId = setInterval(() => {
+            date = new Date(date.getTime() + 1000)
+            const newHours = date.getHours()
+            const newPeriod = newHours >= 12 ? 'pm' : 'am'
+            const newTimePart = `${newHours % 12 || 12}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`
+            setHora(`${newTimePart} ${newPeriod}`)
+          }, 1000)
+
+          return () => {
+            clearInterval(intervalId)
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching time:', error)
+        })
+    }
+
+    fetchTime()
+    const fetchIntervalId = setInterval(fetchTime, 5 * 60 * 1000)
 
     return () => {
-      isMounted.current = false
+      clearInterval(fetchIntervalId)
     }
   }, [])
-
-  const SimularReloj = (hora) => {
-    const [timePart, period] = hora.split(' ')
-    const [hours, minutes, seconds] = timePart.split(':').map(Number)
-    let date = new Date()
-    date.setHours(hours + (period === 'pm' ? 12 : 0))
-    date.setMinutes(minutes)
-    date.setSeconds(seconds)
-
-    const intervalId = setInterval(() => {
-      if (isMounted.current) {
-        date = new Date(date.getTime() + 1000)
-        const newHours = date.getHours()
-        const newPeriod = newHours >= 12 ? 'pm' : 'am'
-        const newTimePart = `${newHours % 12 || 12}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`
-        setHora(`${newTimePart} ${newPeriod}`)
-      }
-    }, 1000)
-
-    return () => {
-      clearInterval(intervalId)
-    }
-  }
 
   return (
     <section className='grid grid-cols-2 w-full sm:text-xs lg:text-lg justify-around py-2 gap-2'>
@@ -65,7 +61,7 @@ export function InfoPuntCoord ({ pdv }) {
         VERSION != 0
           ? (
             <article className='flex justify-center text-center border py-1 rounded-md bg-slate-300 dark:bg-slate-900 font-semibold gap-2'>
-              <p className='text-xs lg:text-sm 2xl:text-xl'>Catergoria: {CATEGORIA || ''} </p>
+              <p className='text-xs lg:text-sm 2xl:text-xl'>Categoria: {CATEGORIA || ''} </p>
               <p className='text-xs lg:text-sm 2xl:text-xl'>Clasificación: 💎 {VERSION || ''} 💎</p>
             </article>
             )
