c675641c31cc1ddae4345185296f472b23227563
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-12 13:02:17 -0500

    estos cambios funcionan correctamente

--- src/Pages/LoginForm.jsx
+++ src/Pages/LoginForm.jsx
@@ -17,8 +17,8 @@ const LoginForm = () => {
 
   const { login } = useAuth()
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
+  const handleSubmit = async (ev) => {
+    ev.preventDefault()
     setLoading(true)
     setMessage('Iniciando Sesión...')
     try {
@@ -45,7 +45,7 @@ const LoginForm = () => {
   return (
     <section className="w-full h-[100vh] flex flex-col items-center justify-center bg-gradient-to-b from-gray-400 to-gray-200">
 
-      <form className='w-96 mb-2 border p-12 rounded-lg bg-white/30 flex flex-col gap-4 shadow-xl'>
+      <form className='w-96 mb-2 border p-12 rounded-lg bg-white/30 flex flex-col gap-4 shadow-xl' onSubmit={(ev) => handleSubmit(ev)}>
         <figure className='flex justify-center'>
           <img src="/gane.png" width={180} />
         </figure>
@@ -54,7 +54,7 @@ const LoginForm = () => {
           <label className='font-semibold'>Usuario</label>
           <div className='flex items-center w-full justify-around gap-4'>
             <UserIcon size={10}/>
-            <input name='username' type='text' placeholder='CV1118*****' autoComplete='username' className='w-full p-2 rounded-md border-none outline-none'
+            <input name='username' type='text' placeholder='CV1118*****' required autoComplete='username' className='w-full p-2 rounded-md border-none outline-none'
               onChange={ev => { setUsername(ev.target.value) }} />
           </div>
         </article>
@@ -63,13 +63,12 @@ const LoginForm = () => {
           <label className='font-semibold'>Contraseña</label>
           <div className='flex items-center w-full justify-around gap-4'>
             <LockIcon size={10}/>
-            <input name='password' type='password' placeholder='**********' autoComplete='username' className='w-full p-2 rounded-md border-none outline-none'
+            <input name='password' type='password' placeholder='**********' required autoComplete='username' className='w-full p-2 rounded-md border-none outline-none'
               onChange={ev => { setPassword(ev.target.value) }} />
           </div>
         </article>
 
-         <button className='p-2 bg-blue-700 rounded-lg text-white font-semibold shadow-xl hover:bg-blue-900 transition duration-300 ease-in-out cursor-pointer'
-          onClick={handleSubmit}>Iniciar Sesión</button>
+         <button className='p-2 bg-blue-700 rounded-lg text-white font-semibold shadow-xl hover:bg-green-500 transition duration-300 ease-in-out cursor-pointer'>Iniciar Sesión</button>
       </form>
 
       <section className='h-10'>
