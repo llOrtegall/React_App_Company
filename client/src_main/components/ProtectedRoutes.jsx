641106ff52e83799e9183944a8163023df65f410
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-30 15:39:28 -0500

    desde 0 el app

--- client/src_main/components/ProtectedRoutes.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-export const ProtectdeRoutes = ({ isAllowed, children, redirectTo = '/login' }) => {
-  if (!isAllowed) return <Navigate to={redirectTo} />
-
-  return (children || <Outlet />)
-}
